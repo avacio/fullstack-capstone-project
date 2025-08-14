04e3f4da243349811000aa8e796b366768597d26
avacio <lexigerv@gmail.com>
2025-08-14 01:37:20 -0700

    Add details to routes

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,22 +4,24 @@ import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
   const navigate = useNavigate();
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/search" element={<SearchPage/>} />
-        </Routes>
-        </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+      </Routes>
+    </>
   );
 }
 export default App;
